40993ce7db091da9c929a4ff31b53b7e3577ae07
Wojtyua <wojciechderlikiewicz@gmail.com>
2023-08-25 15:24:02 +0200

    navigation bar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -2,7 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-import Navbar from "@/components/Navbar";
+import { Navbar } from "@/components/navbar";
 
 export default async function DashboardLayout({
   children,
